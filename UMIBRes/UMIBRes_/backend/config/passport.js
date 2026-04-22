641638a49d12a4e66642053a53fc8a9575a9584e
Ajete Ibishi <ajeteibishi13@gmail.com>
2026-04-22 14:18:40 +0200

    Initial commit

---
+++ UMIBRes/UMIBRes_/backend/config/passport.js
@@ -0,0 +1,46 @@
+/* eslint-env node */
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import db from "./db.js";
+
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+
+  },
+  (accessToken, refreshToken, profile, done) => {
+
+    console.log("Google profile object:", profile);
+    console.log("Google ID:", profile.id);
+    console.log("Email:", profile.emails[0].value);
+    console.log("Display Name:", profile.displayName);
+
+    const email = profile.emails[0].value;
+
+    if (!email.endsWith("@umib.net")) {
+      return done(null, false, { message: "Vetëm email @umib.net lejohet" });
+    }
+
+    db.query(
+      "INSERT INTO users (google_id, email, full_name) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE full_name=?",
+      [profile.id, email, profile.displayName, profile.displayName],
+      (err) => {
+        if (err) return done(err);
+        return done(null, profile);
+      }
+    );
+
+
+  }
+));
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
+
+export default passport;
---
+++ UMIBRes/UMIBRes_/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import LoginForm from "./frontend/LoginForm";
+import HomePage from "./frontend/HomePage";
+import ProfessorDashboard from "./frontend/professor/pages/ProfessorDashboard";
+import ProRectorDashboard from "./frontend/ProRector/pages/ProRectorDashboard";
+import CommitteeDashboard from "./frontend/committee/pages/CommitteeDashboard";
+import NotificationsPage from "./frontend/committee/pages/NotificationsPage";
+import CommitteeSettings from "./frontend/committee/pages/CommitteeSettings";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/professor/dashboard" element={<ProfessorDashboard />} />
+        <Route path="/prorector/dashboard" element={<ProRectorDashboard />} />
+        <Route path="/committee/dashboard" element={<CommitteeDashboard />} />
+        <Route path="/committee/notifications" element={<NotificationsPage />} />
+        <Route path="/committee/settings" element={<CommitteeSettings />} />
+        <Route path="/dashboard" element={<Navigate to="/professor/dashboard" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
